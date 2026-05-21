4503d863bec99d1b69c510dcb1214bcb5c51746a
inara <inaraoenning@gmail.com>
2026-05-21 19:04:03 -0300

    add config cors

--- rpg/rpg/src/main/kotlin/desafio/final/rpg/config/WebScoketConfig.kt
+++ rpg/rpg/src/main/kotlin/desafio/final/rpg/config/WebScoketConfig.kt
@@ -2,6 +2,7 @@ package desafio.final.rpg.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -9,6 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 // @EnableWebSocketMessageBroker: ativa o suporte completo a WebSocket com protocolo STOMP
 // STOMP = Simple text Oriented Messaging Protocol
 @Configuration
+@CrossOrigin(origins = ["*"])
 @EnableWebSocketMessageBroker
 
 class WebScoketConfig : WebSocketMessageBrokerConfigurer {
@@ -41,6 +43,7 @@ class WebScoketConfig : WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/chat-websocket")
             .setAllowedOriginPatterns("*")
             .withSockJS()
+
 
     }
 }
